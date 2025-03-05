4c2f2869f6f293a87c8e3c70d25e0239bf389259
Obinnaojukwu <gd064604@gmail.com>
2025-03-05 03:53:56 +0100

    api

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // const API_URL = process.env.REACT_APP_API_URL;
 
-const API_URL = "https://learnconnect-backend.onrender.com";
+const API_URL = process.env.REACT_APP_API_URL || "https://learnconnect-backend.onrender.com";
 
 const api = axios.create({
     baseURL: API_URL,
