53a5a45516f3e7269429849baf8017cc083ba936
Obinnaojukwu <gd064604@gmail.com>
2025-03-22 17:25:15 +0100

    sync

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,7 @@
 const express = require('express');
-const http = require('http');
-const WebSocket = require('ws');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const path = require('path');
-const history = require('connect-history-api-fallback');
 const authRoutes = require('./routes/authRoutes');
 const userRoutes = require('./routes/userRoutes');
 const audioRoutes = require('./routes/audioRoutes');
@@ -15,24 +12,17 @@ const levelRoutes = require('./routes/levelRoutes');
 const paymentRoutes = require('./routes/paymentRoutes');
 const purchaseRoutes = require('./routes/purchaseRoutes');
 const webhookRoutes = require('./routes/webhookRoutes');
-const messageRoutes = require('./routes/messageRoutes');
 const { notFound, errorHandler } = require('./middleware/errorMiddleware');
-const db = require('./config/database');
 
 dotenv.config();
 
 const app = express();
-const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
-
-// Enable CORS
 app.use(cors());
 app.use(express.json());
 
 // Serve static files from the uploads directory
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-// Register API routes
 app.use('/api/auth', authRoutes);
 app.use('/api/user', userRoutes);
 app.use('/api/audios', audioRoutes);
@@ -43,56 +33,16 @@ app.use('/api/levels', levelRoutes);
 app.use('/api/payment', paymentRoutes);
 app.use('/api/purchase', purchaseRoutes);
 app.use('/api/payment', webhookRoutes);
-app.use('/api/messages', messageRoutes);
 
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'frontend/build')));
 
-// Handle WebSocket connections
-wss.on('connection', (ws) => {
-    console.log('New client connected');
-    
-    ws.on('message', (message) => {
-        const parsedMessage = JSON.parse(message);
-        const { senderId, receiverId, content } = parsedMessage;
-
-        // Save message to the database
-        const sql = `INSERT INTO messages (senderId, receiverId, message) VALUES (?, ?, ?)`;
-        const params = [senderId, receiverId, content];
-        db.run(sql, params, function(err) {
-            if (err) {
-                console.error('Error saving message:', err.message);
-            } else {
-                console.log('Message saved to database');
-            }
-        });
-
-        // Broadcast message to the receiver
-        wss.clients.forEach((client) => {
-            if (client !== ws && client.readyState === WebSocket.OPEN) {
-                client.send(JSON.stringify(parsedMessage));
-            }
-        });
-    });
-
-    ws.on('close', () => {
-        console.log('Client disconnected');
-    });
-});
-
-// History API Fallback
-app.use(history());
 
 // Add a route for the root path
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
+  res.send('Welcome to the API');
 });
-
 
 app.use(notFound);
 app.use(errorHandler);
 
-
-// Start the server
 const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
